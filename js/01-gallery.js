c74f0f878a1a622a1310fad880a0be027bd40dd2
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-03-30 00:34:09 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const container = document.querySelector(.'gallery')
+const galleryContainer = document.querySelector(.'gallery');
+const galleryCardSet = createGallery(galleryItems);
 
+function createGallery(galleryItems){
+    
+}
 console.log(galleryItems);
